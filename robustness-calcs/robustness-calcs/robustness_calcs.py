6c7b7f524fa75d56cfea4d5c8a568b47b5f2b637
cameronmcphail <a1630736@adelaide.edu.au>
2016-11-03 10:09:52 +1030

    Added a function that calls all robustness metrics calcs

--- robustness-calcs/robustness-calcs/robustness_calcs.py
+++ robustness-calcs/robustness-calcs/robustness_calcs.py
@@ -10,15 +10,42 @@ import optimism_pessimism
 import insufficient_reason
 import starrs_domain
 
+def calc_all(performance):
+    """Returns the robustness values for a variety of metrics
+
+    'performance' is a 2D array of the performance metric used to
+        evaluate each solution in each scenario
+        dimension 1 is the solution index
+        dimension 2 is the scenario index
+
+    returns an array of class instances which include the name of the metric
+     and the value of the robustness metric for each solution:
+    [
+        .name = "Metric 1 name"
+        .robustness = [<1D array>],
+
+        .name = "Metric 2 name"
+        .robustness = [<1D array>]
+    ]
+    """
+
+    class robustnessMetric:
+        """Contains the name and values or a robustness metric"""
+        def __init__(self, name, robustness):
+            self.name = name
+            self.robustness = robustness
+
+    robustness = []
+    robustness.append(robustnessMetric("Maximin", maximin.calc(performance)))
+    robustness.append(robustnessMetric("Maximax", maximax.calc(performance)))
+    robustness.append(robustnessMetric("Optimism-Pessimism", optimism_pessimism.calc(performance)))
+    robustness.append(robustnessMetric("Principle of Insufficient Reason", insufficient_reason.calc(performance)))
+    robustness.append(robustnessMetric("Starr's Domain Criterion", starrs_domain.calc(performance, 0.95)))
+    
+    return robustness
+
 if __name__ == "__main__":
     performance = [[0.7, 0.8, 0.9], [0.8, 0.9, 1.0]]
-    robustness = maximin.calc(performance)
-    print(robustness)
-    robustness = maximax.calc(performance)
-    print(robustness)
-    robustness = optimism_pessimism.calc(performance)
-    print(robustness)
-    robustness = insufficient_reason.calc(performance)
-    print(robustness)
-    robustness = starrs_domain.calc(performance, 0.95)
-    print(robustness)
+    robustness = calc_all(performance)
+    for metric in robustness:
+        print(metric.name, metric.robustness)
